0f8f4ed04c6ac3876dab789e41dc9d0c385276d6
Caique Nunes <cainunes@mercadolivre.com>
2022-01-04 09:28:56 -0300

    adjust test params

--- src/entity/object-mother-entity.go
+++ src/entity/object-mother-entity.go
@@ -6,98 +6,98 @@ const (
 )
 
 type testScenario struct {
-	testName    string
-	phone       string
-	countryCode string
-	expect      string
+	TestName     string
+	Phone        string
+	CountryCode  string
+	ExpectResult string
 }
 
 func MakeScenarioWithValidPhoneFromCameroon() *testScenario {
 	return &testScenario{
-		testName:    "Valid phone from Cameroon",
-		phone:       "(237) 673122155",
-		countryCode: CodeCameroon,
-		expect:      valid,
+		TestName:     "Valid Phone from Cameroon",
+		Phone:        "(237) 673122155",
+		CountryCode:  CodeCameroon,
+		ExpectResult: valid,
 	}
 }
 
 func MakeScenarioWithInvalidPhoneFromCameroon() *testScenario {
 	return &testScenario{
-		testName:    "Invalid phone from Cameroon",
-		phone:       "(237) 6622284920",
-		countryCode: CodeCameroon,
-		expect:      invalid,
+		TestName:     "Invalid Phone from Cameroon",
+		Phone:        "(237) 6622284920",
+		CountryCode:  CodeCameroon,
+		ExpectResult: invalid,
 	}
 }
 
 func MakeScenarioWithValidPhoneFromEthiopia() *testScenario {
 	return &testScenario{
-		testName:    "Valid phone from Ethiopia",
-		phone:       "(251) 911168450",
-		countryCode: CodeEthiopia,
-		expect:      valid,
+		TestName:     "Valid Phone from Ethiopia",
+		Phone:        "(251) 911168450",
+		CountryCode:  CodeEthiopia,
+		ExpectResult: valid,
 	}
 }
 
 func MakeScenarioWithInvalidPhoneFromEthiopia() *testScenario {
 	return &testScenario{
-		testName:    "Invalid phone from Ethiopia",
-		phone:       "(251) 9119454961",
-		countryCode: CodeEthiopia,
-		expect:      invalid,
+		TestName:     "Invalid Phone from Ethiopia",
+		Phone:        "(251) 9119454961",
+		CountryCode:  CodeEthiopia,
+		ExpectResult: invalid,
 	}
 }
 
 func MakeScenarioWithValidPhoneFromMorocco() *testScenario {
 	return &testScenario{
-		testName:    "Valid phone from Morocco",
-		phone:       "(212) 633963130",
-		countryCode: CodeMorocco,
-		expect:      valid,
+		TestName:     "Valid Phone from Morocco",
+		Phone:        "(212) 633963130",
+		CountryCode:  CodeMorocco,
+		ExpectResult: valid,
 	}
 }
 
 func MakeScenarioWithInvalidPhoneFromMorocco() *testScenario {
 	return &testScenario{
-		testName:    "Invalid phone from Morocco",
-		phone:       "(212) 6007989253",
-		countryCode: CodeMorocco,
-		expect:      invalid,
+		TestName:     "Invalid Phone from Morocco",
+		Phone:        "(212) 6007989253",
+		CountryCode:  CodeMorocco,
+		ExpectResult: invalid,
 	}
 }
 
 func MakeScenarioWithValidPhoneFromMozambique() *testScenario {
 	return &testScenario{
-		testName:    "Valid phone from Mozambique",
-		phone:       "(258) 823747618",
-		countryCode: CodeMozambique,
-		expect:      valid,
+		TestName:     "Valid Phone from Mozambique",
+		Phone:        "(258) 823747618",
+		CountryCode:  CodeMozambique,
+		ExpectResult: valid,
 	}
 }
 
 func MakeScenarioWithInvalidPhoneFromMozambique() *testScenario {
 	return &testScenario{
-		testName:    "Invalid phone from Mozambique",
-		phone:       "(258) 042423566",
-		countryCode: CodeMozambique,
-		expect:      invalid,
+		TestName:     "Invalid Phone from Mozambique",
+		Phone:        "(258) 042423566",
+		CountryCode:  CodeMozambique,
+		ExpectResult: invalid,
 	}
 }
 
 func MakeScenarioWithValidPhoneFromUganda() *testScenario {
 	return &testScenario{
-		testName:    "Valid phone from Uganda",
-		phone:       "(256) 704244430",
-		countryCode: CodeUganda,
-		expect:      valid,
+		TestName:     "Valid Phone from Uganda",
+		Phone:        "(256) 704244430",
+		CountryCode:  CodeUganda,
+		ExpectResult: valid,
 	}
 }
 
 func MakeScenarioWithInvalidPhoneFromUganda() *testScenario {
 	return &testScenario{
-		testName:    "Invalid phone from Uganda",
-		phone:       "(256) 3142345678",
-		countryCode: CodeUganda,
-		expect:      invalid,
+		TestName:     "Invalid Phone from Uganda",
+		Phone:        "(256) 3142345678",
+		CountryCode:  CodeUganda,
+		ExpectResult: invalid,
 	}
 }
