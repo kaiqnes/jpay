03499dc5f084d4ce8534fa6318968569a25f1de5
Caique Nunes <cainunes@mercadolivre.com>
2022-01-06 16:54:01 -0300

    integrate front-end with back-end and implement filters

--- src/views/js/app.jsx
+++ src/views/js/app.jsx
@@ -1,333 +1,141 @@
 class Table extends React.Component {
-    getCustomers() {
-        return [
-            {
-                "customer_name": "Walid Hammadi",
-                "country_name": "Morocco",
-                "country_code": "212",
-                "phone_number": "6007989253",
-                "status": "Invalid"
-            },
-            {
-                "customer_name": "Soufiane Fritisse ",
-                "country_name": "Morocco",
-                "country_code": "212",
-                "phone_number": "633963130",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Nada Sofie",
-                "country_name": "Morocco",
-                "country_code": "212",
-                "phone_number": "654642448",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Younes Boutikyad",
-                "country_name": "Morocco",
-                "country_code": "212",
-                "phone_number": "6546545369",
-                "status": "Invalid"
-            },
-            {
-                "customer_name": "Houda Houda",
-                "country_name": "Morocco",
-                "country_code": "212",
-                "phone_number": "6617344445",
-                "status": "Invalid"
-            },
-            {
-                "customer_name": "Chouf Malo",
-                "country_name": "Morocco",
-                "country_code": "212",
-                "phone_number": "691933626",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Yosaf Karrouch",
-                "country_name": "Morocco",
-                "country_code": "212",
-                "phone_number": "698054317",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "William Kemfang",
-                "country_name": "Cameroon",
-                "country_code": "237",
-                "phone_number": "6622284920",
-                "status": "Invalid"
-            },
-            {
-                "customer_name": "Louis Parfait Ombes Ntso",
-                "country_name": "Cameroon",
-                "country_code": "237",
-                "phone_number": "673122155",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Michael Michael",
-                "country_name": "Cameroon",
-                "country_code": "237",
-                "phone_number": "677046616",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Sugar Starrk Barragan",
-                "country_name": "Cameroon",
-                "country_code": "237",
-                "phone_number": "6780009592",
-                "status": "Invalid"
-            },
-            {
-                "customer_name": "Dominique Mekontchou",
-                "country_name": "Cameroon",
-                "country_code": "237",
-                "phone_number": "691816558",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Joseph Felicien Nomo",
-                "country_name": "Cameroon",
-                "country_code": "237",
-                "phone_number": "695539786",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Thomas Wilfried Lomo Lomo",
-                "country_name": "Cameroon",
-                "country_code": "237",
-                "phone_number": "696443597",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Emile Christian Koukou Dikanda Honore ",
-                "country_name": "Cameroon",
-                "country_code": "237",
-                "phone_number": "697151594",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Nelson Nelson",
-                "country_name": "Cameroon",
-                "country_code": "237",
-                "phone_number": "699209115",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Arreymanyor Roland Tabot",
-                "country_name": "Cameroon",
-                "country_code": "237",
-                "phone_number": "6A0311634",
-                "status": "Invalid"
-            },
-            {
-                "customer_name": "Yonatan Tekelay",
-                "country_name": "Ethiopia",
-                "country_code": "251",
-                "phone_number": "911168450",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Abraham Negash",
-                "country_name": "Ethiopia",
-                "country_code": "251",
-                "phone_number": "911203317",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Zekarias Kebede",
-                "country_name": "Ethiopia",
-                "country_code": "251",
-                "phone_number": "9119454961",
-                "status": "Invalid"
-            },
-            {
-                "customer_name": "Ephrem Kinfe",
-                "country_name": "Ethiopia",
-                "country_code": "251",
-                "phone_number": "914148181",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Filimon Embaye",
-                "country_name": "Ethiopia",
-                "country_code": "251",
-                "phone_number": "914701723",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Fanetahune Abaia",
-                "country_name": "Ethiopia",
-                "country_code": "251",
-                "phone_number": "924418461",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Karim Niki",
-                "country_name": "Ethiopia",
-                "country_code": "251",
-                "phone_number": "966002259",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Shop23 Sales",
-                "country_name": "Ethiopia",
-                "country_code": "251",
-                "phone_number": "9773199405",
-                "status": "Invalid"
-            },
-            {
-                "customer_name": "Frehiwot Teka",
-                "country_name": "Ethiopia",
-                "country_code": "251",
-                "phone_number": "988200000",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Pt Shop 0901 Ultimo ",
-                "country_name": "Uganda",
-                "country_code": "256",
-                "phone_number": "3142345678",
-                "status": "Invalid"
-            },
-            {
-                "customer_name": "Vineet Seth",
-                "country_name": "Uganda",
-                "country_code": "256",
-                "phone_number": "704244430",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Daniel Makori",
-                "country_name": "Uganda",
-                "country_code": "256",
-                "phone_number": "714660221",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Kiwanuka Budallah",
-                "country_name": "Uganda",
-                "country_code": "256",
-                "phone_number": "7503O6263",
-                "status": "Invalid"
-            },
-            {
-                "customer_name": "Jokkene Richard",
-                "country_name": "Uganda",
-                "country_code": "256",
-                "phone_number": "7734127498",
-                "status": "Invalid"
-            },
-            {
-                "customer_name": "Jackson Nelly",
-                "country_name": "Uganda",
-                "country_code": "256",
-                "phone_number": "775069443",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Ogwal David",
-                "country_name": "Uganda",
-                "country_code": "256",
-                "phone_number": "7771031454",
-                "status": "Invalid"
-            },
-            {
-                "customer_name": "Solo Dolo",
-                "country_name": "Mozambique",
-                "country_code": "258",
-                "phone_number": "042423566",
-                "status": "Invalid"
-            },
-            {
-                "customer_name": "Pedro B 173",
-                "country_name": "Mozambique",
-                "country_code": "258",
-                "phone_number": "823747618",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Tanvi Sachdeva",
-                "country_name": "Mozambique",
-                "country_code": "258",
-                "phone_number": "84330678235",
-                "status": "Invalid"
-            },
-            {
-                "customer_name": "Walla'S Singz Junior",
-                "country_name": "Mozambique",
-                "country_code": "258",
-                "phone_number": "846565883",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Florencio Samuel",
-                "country_name": "Mozambique",
-                "country_code": "258",
-                "phone_number": "847602609",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Edunildo Gomes Alberto ",
-                "country_name": "Mozambique",
-                "country_code": "258",
-                "phone_number": "847651504",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Ezequiel Fenias",
-                "country_name": "Mozambique",
-                "country_code": "258",
-                "phone_number": "848826725",
-                "status": "Valid"
-            },
-            {
-                "customer_name": "Sevilton Sylvestre",
-                "country_name": "Mozambique",
-                "country_code": "258",
-                "phone_number": "849181828",
-                "status": "Valid"
+    constructor(props) {
+        super(props)
+        this.state = {
+            customers: null,
+            availableCountries: [],
+            availableStatus: [],
+            filters: {
+                country_name: '',
+                status: ''
             }
-        ]
+        }
+        this.getCustomers()
+    }
+
+    getCustomers(params = '') {
+        fetch('http://localhost:8080/customers/search' + params)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    customers: data.customers,
+                    availableCountries: [...new Set(data.customers.map(item => item.country_name))],
+                    availableStatus: [...new Set(data.customers.map(item => item.status))],
+                    filters: this.state.filters
+                })
+            });
+    }
+
+    filterByCountry = (event) => {
+        let searchParam
+        let country = event.target.value
+
+        this.setState({
+            customers: this.state.customers,
+            availableCountries: this.state.availableCountries,
+            availableStatus: this.state.availableStatus,
+            filters: {
+                country_name: country,
+                status: this.state.filters.status
+            }
+        })
+
+        if (country === '') {
+            searchParam = this.state.filters.status === '' ? '' : '?status=' + this.state.filters.status
+        } else {
+            let statusFilter = this.state.filters.status
+            searchParam = statusFilter === '' ? '?country_name=' + country : '?status=' + statusFilter + '&country_name=' + country
+        }
+
+        this.getCustomers(searchParam)
+    }
+
+    filterByStatus = (event) => {
+        let searchParam
+        let status = event.target.value
+
+        this.setState({
+            customers: this.state.customers,
+            availableCountries: this.state.availableCountries,
+            availableStatus: this.state.availableStatus,
+            filters: {
+                country_name: this.state.filters.country_name,
+                status: status
+            }
+        })
+
+        if (status === '') {
+            searchParam = this.state.filters.country_name === '' ? '' : '?country_name=' + this.state.filters.country_name
+        } else {
+            let countryNameFilter = this.state.filters.country_name
+            searchParam = countryNameFilter === '' ? '?status=' + status : '?status=' + status + '&country_name=' + countryNameFilter
+        }
+
+        this.getCustomers(searchParam)
     }
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-xs-8 col-xs-offset-2 jumbotron text-center">
-                        <h1>Customers</h1>
-                        <div className="table-responsive">
-                            <table class="table">
-                                <thead>
-                                <tr>
-                                    <th>Customer Name</th>
-                                    <th>Country Name</th>
-                                    <th>Country Code</th>
-                                    <th>Phone Number</th>
-                                    <th>Status</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                {this.getCustomers().map(item => {
-                                    return (
-                                        <tr key={item.phone_number}>
-                                            <td>{ item.customer_name }</td>
-                                            <td>{ item.country_name }</td>
-                                            <td>{ item.country_code }</td>
-                                            <td>{ item.phone_number }</td>
-                                            <td>{ item.status }</td>
-                                        </tr>
-                                    );
-                                })}
-                                </tbody>
-                            </table>
+            this.state.customers ?
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xs-8 col-xs-offset-2 jumbotron text-center">
+                            <h1>Customers</h1>
+                            <div>
+                                <div>
+                                    Status
+                                    <select onChange={this.filterByStatus}>
+                                        <option></option>
+                                        {this.state.availableStatus.map(item => {
+                                            return (
+                                                <option>{item}</option>
+                                            );
+                                        })}
+                                    </select>
+                                </div><div>
+                                Country
+                                <select onChange={this.filterByCountry}>
+                                    <option></option>
+                                    {this.state.availableCountries.map(item => {
+                                        return (
+                                            <option>{item}</option>
+                                        );
+                                    })}
+                                </select>
+                            </div>
+                            </div>
+                            <div className="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th>Customer Name</th>
+                                        <th>Country Name</th>
+                                        <th>Country Code</th>
+                                        <th>Phone Number</th>
+                                        <th>Status</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {this.state.customers.map(item => {
+                                        return (
+                                            <tr key={item.phone_number}>
+                                                <td>{item.customer_name}</td>
+                                                <td>{item.country_name}</td>
+                                                <td>{item.country_code}</td>
+                                                <td>{item.phone_number}</td>
+                                                <td>{item.status}</td>
+                                            </tr>
+                                        );
+                                    })}
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                : <p>Loading</p>
         );
     }
 }
 
-ReactDOM.render(<Table />, document.getElementById("app"));
+ReactDOM.render(<Table/>, document.getElementById("app"));
