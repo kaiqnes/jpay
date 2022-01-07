c14adc9b13b8fef5d94287942bf6a0b70d49b1dc
Caique Nunes <cainunes@mercadolivre.com>
2022-01-07 08:10:00 -0300

    change total from int64 to int

--- src/dto/customer-dto.go
+++ src/dto/customer-dto.go
@@ -7,7 +7,7 @@ import (
 type CustomerOutputDto struct {
 	Limit     int        `json:"limit"`
 	Offset    int        `json:"offset"`
-	Total     int64      `json:"total"`
+	Total     int        `json:"total"`
 	Customers []Customer `json:"customers"`
 }
 
